41538aa34685fa48c8b85c18513ac021891d9866
andy1729 <ujjawalanand1729@gmail.com>
2020-04-18 23:12:22 +0530

    added views, urls, forms related to user

---
+++ champsquarebackend/users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+    user_detail_view,
+    user_update_view,
+    user_redirect_view
+)
+
+app_name = "users"
+
+urlpatterns = [
+    path('~redirect/', user_redirect_view, name="redirect"),
+    path('~update/', user_update_view, name="update"),
+    path('<str:username>', user_detail_view, name="detail")
+]
