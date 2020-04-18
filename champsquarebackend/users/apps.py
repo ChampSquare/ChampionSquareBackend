6e867795f082c3bec31ad375af3e966bdc602162
andy1729 <ujjawalanand1729@gmail.com>
2020-04-18 21:43:01 +0530

    created custom user model

--- champsquarebackend/users/apps.py
+++ champsquarebackend/users/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class UsersConfig(AppConfig):
-    name = 'users'
+    name = 'champsquarebackend.users'
+    verbose_name = _("Users")
+
+    def ready(self):
+        try:
+            import champsquarebackend.users.signals
+        except ImportError:
+            pass
--- champsquarebackend/users/models.py
+++ champsquarebackend/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(_("Name of the user"), blank=True, max_length=100)
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
+    
