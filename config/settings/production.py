a15399b40f0ac1dc509ba407885486034e2c4432
andy1729 <ujjawalanand1729@gmail.com>
2020-05-03 13:15:54 +0530

    added secret key

--- config/settings/production.py
+++ config/settings/production.py
@@ -288,7 +288,7 @@ ACCOUNT_SECURITY_API_KEY = env('ACCOUNT_SECURITY_API_KEY')
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-# SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = "bfbeffgbwy56736bvnbnb736"
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
 
