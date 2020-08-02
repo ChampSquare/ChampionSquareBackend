6247b77fea058fd69fac2e3fea95015cacc86e05
andy1729 <ujjawalanand1729@gmail.com>
2020-08-02 20:34:04 +0530

    fixing bug in celery

--- champsquarebackend/apps/dashboard/records/tasks.py
+++ champsquarebackend/apps/dashboard/records/tasks.py
@@ -4,4 +4,5 @@ from champsquarebackend.apps.dashboard.records.utils import post_process_video
 
 @shared_task
 def process_video(video_record_id):
-    return post_process_video(video_record_id)
+    pass
+    #return post_process_video(video_record_id)
